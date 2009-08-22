d2d313f03651a7330b98c099c27ef0c94332f96c
Jamie Dyer <jamie@kernowsoul.com>
2009-08-22 06:10:12 +0100

    fixed theos grave merging mishap

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,7 @@ ActionController::Routing::Routes.draw do |map|
     league.resources :matches, :except => [:index, :destroy], :member => {:full_time => :put}
   end
   
-<<<<<<< HEAD:config/routes.rb
-=======
   map.resources :users, :only => [:new, :create] do |user|
     user.resources :leagues, :only => :index
   end
->>>>>>> c6bbbb2c7a91d563d7a59021e93fd433d0d3c0e7:config/routes.rb
 end
