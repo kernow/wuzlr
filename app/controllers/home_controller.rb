959e3e084c2708fbe311c267e5db1cf0dbca7877
Jamie Dyer <jamie@kernowsoul.com>
2009-08-22 21:21:31 +0100

    set flashed on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   
   def index
-    
+    flash[:notice] = "YAHOOOOO notice"
+    flash[:success] = "YAHOOOOO success"
+    flash[:error] = "YAHOOOOO error"
   end
   
 end
