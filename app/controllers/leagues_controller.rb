00ef5d7ae113838267f32a801303b569ad112249
Jamie Dyer <jamie@kernowsoul.com>
2009-08-22 02:14:54 +0100

    league creation

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,55 +1,45 @@
 class LeaguesController < ApplicationController
-  # GET /leagues
-  # GET /leagues.xml
-  def index
-    @leagues = League.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @leagues }
-    end
-  end
 
   # GET /leagues/1
   # GET /leagues/1.xml
   def show
-    @leagues = League.find(params[:id])
+    @league = League.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @leagues }
+      format.xml  { render :xml => @league }
     end
   end
 
   # GET /leagues/new
   # GET /leagues/new.xml
   def new
-    @leagues = League.new
+    @league = League.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @leagues }
+      format.xml  { render :xml => @league }
     end
   end
 
   # GET /leagues/1/edit
   def edit
-    @leagues = League.find(params[:id])
+    @league = League.find(params[:id])
   end
 
   # POST /leagues
   # POST /leagues.xml
   def create
-    @leagues = League.new(params[:leagues])
+    @league = League.new(params[:league])
 
     respond_to do |format|
-      if @leagues.save
+      if @league.save
         flash[:notice] = 'League was successfully created.'
-        format.html { redirect_to(@leagues) }
-        format.xml  { render :xml => @leagues, :status => :created, :location => @leagues }
+        format.html { redirect_to(@league) }
+        format.xml  { render :xml => @league, :status => :created, :location => @league }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @leagues.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @league.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -57,16 +47,16 @@ class LeaguesController < ApplicationController
   # PUT /leagues/1
   # PUT /leagues/1.xml
   def update
-    @leagues = League.find(params[:id])
+    @league = League.find(params[:id])
 
     respond_to do |format|
-      if @leagues.update_attributes(params[:leagues])
+      if @league.update_attributes(params[:league])
         flash[:notice] = 'League was successfully updated.'
-        format.html { redirect_to(@leagues) }
+        format.html { redirect_to(@league) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @leagues.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @league.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -74,8 +64,8 @@ class LeaguesController < ApplicationController
   # DELETE /leagues/1
   # DELETE /leagues/1.xml
   def destroy
-    @leagues = League.find(params[:id])
-    @leagues.destroy
+    @league = League.find(params[:id])
+    @league.destroy
 
     respond_to do |format|
       format.html { redirect_to(leagues_url) }
