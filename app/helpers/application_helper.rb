40f5908fcca431bcc79da1b919bfb6fa8eebf607
Jamie Dyer <jamie@kernowsoul.com>
2009-09-08 22:43:38 +0100

    removed logger code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,14 +15,11 @@ module ApplicationHelper
   def team_flag_image_for(player, position, total_players, teams)
     team = case
       when position == 0
-        logger.info "team numnber: first"
         teams.first
       when position == total_players - 1
-        logger.info "team numnber: last"
         teams.last
       else
         inc = (teams.size / (total_players - 1)).floor
-        logger.info "team number: #{inc * position}"
         teams[inc * position]
     end
     image_tag team[:flag], :title => team[:name]
